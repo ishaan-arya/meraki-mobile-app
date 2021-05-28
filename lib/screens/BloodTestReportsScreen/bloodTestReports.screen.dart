146a58b77264ff35168c75f2622e33d5a0c5fe47
Ishaan <ishaanarya2004@gmail.com>
2021-05-28 23:42:02 +0530

    ft[screens/BloodTestReportsScreen] => 📝 Create the blood test report screen with the date picker

--- lib/screens/BloodTestReportsScreen/bloodTestReports.screen.dart
+++ lib/screens/BloodTestReportsScreen/bloodTestReports.screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meraki/widgets/homeButton.widget.dart';
+import 'package:meraki/utils/constants.dart';
+import 'package:date_field/date_field.dart';
 
 class BloodTestReportsScreen extends StatefulWidget {
   static String id = "bloodTestReportsScreen_id";
@@ -15,11 +17,40 @@ class _BloodTestReportsScreenState extends State<BloodTestReportsScreen> {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             HomeButton(
               onTap: () {
                 Navigator.pop(context);
               },
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            Center(
+              child: Text(
+                'BLOOD TEST REPORTS',
+                style: kHeadingTextStyle,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            Divider(thickness: 1.5, color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: DateTimeFormField(
+                decoration: const InputDecoration(
+                  hintStyle: TextStyle(color: Colors.black),
+                  errorStyle: TextStyle(color: Colors.redAccent),
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.event_note),
+                  labelText: 'Date',
+                ),
+                mode: DateTimeFieldPickerMode.date,
+                autovalidateMode: AutovalidateMode.always,
+                validator: (e) =>
+                    (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                onDateSelected: (DateTime value) {
+                  print(value);
+                },
+              ),
             ),
           ],
         ),
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+//colors
 const Color kPrimaryColor = Color(0xFFFEE1D3);
 const Color kSecondaryColor = Color(0xFFFB9343);
 
+//text styles
 const kMenuTextStyle = TextStyle(
   color: Colors.white,
   fontSize: 20,
+  fontFamily: 'Montserrat',
+);
+
+const kHeadingTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 30,
   fontFamily: 'Montserrat',
 );
