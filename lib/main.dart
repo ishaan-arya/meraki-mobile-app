04c837ca8c7b6baec83e4290152b185f0245ca61
Ishaan <ishaanarya2004@gmail.com>
2021-08-18 14:07:43 +0530

    Revert "ft[core] => 🔥completed firebase initial setup"
    This reverts commit 1a56cf805d1faddf2867e23f18b276c551baa6ea.

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,8 @@ import 'package:meraki/screens/user/SignUpScreen/signUp.screen.dart';
 import 'package:meraki/screens/user/SignInScreen/signIn.screen.dart';
 import 'package:meraki/screens/admin/AdminMenuScreen/adminMenu.screen.dart';
 import 'package:meraki/utils/constants.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(Meraki_App());
 }
 
@@ -33,7 +30,7 @@ class Meraki_App extends StatelessWidget {
         //Admin Screens
         AdminMenuScreen.id: (context) => AdminMenuScreen(),
       },
-      initialRoute: SignUpScreen.id,
+      initialRoute: AdminMenuScreen.id,
     );
   }
 }
